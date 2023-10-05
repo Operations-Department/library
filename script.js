71e3a1147c3d4820b3a3f148e3db6f317f29bd43
Chris <129601042+Operations-Department@users.noreply.github.com>
2023-10-05 19:58:22 +0900

    Add style to form, add event to receive and store data into library array

--- script.js
+++ script.js
@@ -1,37 +1,52 @@
 const addBookText = document.getElementById('add-book-text');
 const addBookButton = document.getElementById('add-book-button');
+const bookFormContainer = document.getElementById('book-form-container');
 const bookForm = document.getElementById('book-form');
 const bookFormCancelButton = document.getElementById('cancel');
+const bookFormSubmitButton = document.getElementById('submit');
 
-addBookButton.addEventListener('mouseover', function () {
+addBookButton.addEventListener('mouseover', function() {
     addBookText.style.display = 'block';
 })
-addBookButton.addEventListener('mouseout', function () {
+addBookButton.addEventListener('mouseout', function() {
     addBookText.style.display = '';
 })
-addBookButton.addEventListener('click', function () {
-    bookForm.style.display = 'block';
+addBookButton.addEventListener('click', function() {
+    bookFormContainer.style.display = 'block';
     addBookButton.style.display = 'none';
 })
-bookFormCancelButton.addEventListener('click', function () {
-    bookForm.style.display = '';
+bookFormCancelButton.addEventListener('click', function() {
+    bookFormContainer.style.display = '';
+    addBookButton.style.display = '';
+})
+bookFormSubmitButton.addEventListener('click', function(event) {
+    event.preventDefault(); //prevent from from submitting and refreshing page
+    const title = document.getElementById('title').value;
+    const author = document.getElementById('author').value;  // attach variables to form elements
+    const pages = document.getElementById('pages').value;
+    const read = document.getElementById('read-status').checked;
+
+    //create a new Book object and add to library
+    const newBook = new Book(title, author, pages, read);
+    addBookToLibrary(newBook);
+
+    //clear the form
+    bookForm.reset();
+    bookFormContainer.style.display = '';
     addBookButton.style.display = '';
 })
 
-
-
-const myLibrary = [];
-
+//Book constructor
 function Book(title, author, pages, read) {
     this.title = title
     this.author = author
     this.pages = pages 
     this.read = read
-    this.info = function () {
-        return(`${this.title} by ${this.author}, ${this.pages}, ${this.read}`);
-    }
 }
 
-function addBookToLibrary() {
-    //do stuff
+const myLibrary = [];
+
+function addBookToLibrary(book) {
+    myLibrary.push(book);
+    //update display of library here
 }
