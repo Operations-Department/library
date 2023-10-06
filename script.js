48003f451b6d5d00a93da0e24658c928b386dc47
Chris <129601042+Operations-Department@users.noreply.github.com>
2023-10-06 19:51:01 +0900

    add function to displaynew books in page, add dom css elements

--- script.js
+++ script.js
@@ -1,11 +1,13 @@
-const addBookText = document.getElementById('add-book-text');
+const addBookText = document.getElementById('add-book-text'); // connect html elements to the dom
 const addBookButton = document.getElementById('add-book-button');
 const bookFormContainer = document.getElementById('book-form-container');
 const bookForm = document.getElementById('book-form');
 const bookFormCancelButton = document.getElementById('cancel');
 const bookFormSubmitButton = document.getElementById('submit');
+const userCardContainer = document.getElementById('card-container');
+const userCard = document.getElementById('user-card');
 
-addBookButton.addEventListener('mouseover', function() {
+addBookButton.addEventListener('mouseover', function() { //events to trigger form appearance
     addBookText.style.display = 'block';
 })
 addBookButton.addEventListener('mouseout', function() {
@@ -18,6 +20,7 @@ addBookButton.addEventListener('click', function() {
 bookFormCancelButton.addEventListener('click', function() {
     bookFormContainer.style.display = '';
     addBookButton.style.display = '';
+    bookForm.reset();
 })
 bookFormSubmitButton.addEventListener('click', function(event) {
     event.preventDefault(); //prevent from from submitting and refreshing page
@@ -29,11 +32,13 @@ bookFormSubmitButton.addEventListener('click', function(event) {
     //create a new Book object and add to library
     const newBook = new Book(title, author, pages, read);
     addBookToLibrary(newBook);
+    displayBooks();
 
     //clear the form
     bookForm.reset();
     bookFormContainer.style.display = '';
     addBookButton.style.display = '';
+    console.log(myLibrary);
 })
 
 //Book constructor
@@ -45,8 +50,62 @@ function Book(title, author, pages, read) {
 }
 
 const myLibrary = [];
+//////////////////////////////////////////////// test books
+const initialBook = new Book("The Philosopher's Stone", "J.K. Rowling", 223, true);
+addBookToLibrary(initialBook);
+const secondBook = new Book("The Chamber of Secrets", "J.K. Rowling", 384, false);
+addBookToLibrary(secondBook);
+const third = new Book("The Prisoner of Azkaban", "J.K. Rowling", 317, true);
+addBookToLibrary(third);
+const fourth = new Book("The Goblet of Fire", "J.K. Rowling", 636, false);
+addBookToLibrary(fourth);
+displayBooks();
+////////////////////////////////////////////////
 
 function addBookToLibrary(book) {
     myLibrary.push(book);
-    //update display of library here
 }
+
+function displayBooks() {
+    userCardContainer.innerHTML = '';
+
+    for (i = 0; i < myLibrary.length; i++) {
+        if (myLibrary.length <= 0) return
+        if (myLibrary.length > 0) {
+            const book = myLibrary[i];
+            
+            const bookDiv = document.createElement('div');
+
+            const bookTitle = document.createElement('h2');
+            bookTitle.textContent = `"${book.title}"`;
+
+            const bookAuthor = document.createElement('h2');
+            bookAuthor.textContent = `by ${book.author}`;
+
+            const bookPages = document.createElement('h2');
+            bookPages.textContent = `${book.pages} pages`;
+
+            const domReadButton = document.createElement('button');
+            domReadButton.textContent = 'Read';
+
+            const domRemoveButton = document.createElement('button');
+            domRemoveButton.textContent = 'Remove';
+
+            bookDiv.classList.add('domBookCard');
+            domReadButton.classList.add('domReadButton');
+            domReadButton.classList.add('button');
+            domRemoveButton.classList.add('domRemoveButton');
+            domRemoveButton.classList.add('button');
+
+            userCardContainer.appendChild(bookDiv);
+            bookDiv.appendChild(bookTitle);
+            bookDiv.appendChild(bookAuthor);
+            bookDiv.appendChild(bookPages);
+            bookDiv.appendChild(domReadButton);
+            bookDiv.appendChild(domRemoveButton);
+        }
+    }
+}
+
+// add read button toggle
+// add function to remove button to remove object
